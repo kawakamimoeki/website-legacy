5e45459737d28b45616befdec910ecd60f54041e
cc-kawakami <me@moeki.dev>
2022-11-20 20:38:33 +0900

    feat: improve style

--- components/post-list.jsx
+++ components/post-list.jsx
@@ -7,14 +7,14 @@ export default function Component({ posts }) {
     <ul>
       {posts.map(({ id, date, title, content }) => (
         <li className="py-5 border-2 p-6 my-4 rounded-md" key={id}>
-          <h3 className="text-3xl font-bold">
-            <Link className="underline" href={`/blog/${id}`}>
+          <h3 className="text-xl font-bold">
+            <Link href={`/blog/${id}`}>
               {title}
             </Link>
           </h3>
-          <Date className="text-sm opacity-50" dateString={date}></Date>
-          <Markdown limit={256} content={content}></Markdown>
-          <div className="mt-3 text-end">
+          <Date className="text-xs opacity-50" dateString={date}></Date>
+          <Markdown className="text-xs" limit={256} content={content}></Markdown>
+          <div className="mt-1 text-end">
             <Link
               className="underline decoration-yellow-400 decoration-2 underline-offset-4"
               href={`/blog/${id}`}>
