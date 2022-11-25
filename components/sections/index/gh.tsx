6d3d13283131d22a0f2e5ca776bc57dd84c82b82
cc-kawakami <me@moeki.dev>
2022-11-26 04:26:16 +0900

    fix: add target to link

--- components/sections/index/gh.tsx
+++ components/sections/index/gh.tsx
@@ -1,11 +1,12 @@
+import Link from 'next/link'
 import React from 'react'
 import { GoMarkGithub } from 'react-icons/go'
 
 export default function Gh({ repo }) {
   return (
-    <a href={repo.url}>
+    <Link href={repo.url} target="_blank" rel="noopener noreferer">
       <GoMarkGithub className="inline mr-2" />
       {repo.nameWithOwner}: {repo.description}
-    </a>
+    </Link>
   )
 }
