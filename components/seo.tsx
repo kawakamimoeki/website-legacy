14ad6aad1e3f064db21240e07247f99c55923ecb
kawakamimoeki <hello@kawakamimoeki.com>
2022-12-01 13:50:00 +0900

    fix: head

--- components/seo.tsx
+++ components/seo.tsx
@@ -7,7 +7,7 @@ export default function Seo() {
   return (
     <Head>
       <title>{t.title}</title>
-      <link rel="alternate" hrefLang="en" href="https://kawakamimoeki.com" />
+      <link rel="alternate" hrefLang="en-us" href="https://kawakamimoeki.com" />
       <link rel="alternate" hrefLang="ja" href="https://kawakamimoeki.com/ja" />
       <meta name="description" content={t.description} />
       <meta property="og:url" content="https://kawakamimoeki.com" />
