f668b4b067c6ebe707c3da0fba03e08f532f2020
Moeki Kawakami <me@moeki.dev>
2023-08-21 22:39:07 +0900

    Add feed

---
+++ lib/get-feed.ts
@@ -0,0 +1,28 @@
+import RSS from 'rss';
+import { getPosts } from './get-posts';
+
+export const getFeed = async () => {
+  const feed = new RSS({
+    title: 'moeki.dev',
+    description: 'Moeki Kawakami\'s blog',
+    feed_url: 'https://moeki.dev/feed',
+    site_url: 'https://moeki.dev',
+    image_url: 'https://moeki.dev/images/moeki.svg',
+    managingEditor: 'Moeki Kawakami',
+    webMaster: 'Moeki Kawakami',
+    copyright: '2023 Moeki Kawakami',
+    language: 'ja',
+  });
+
+  const posts = await getPosts();
+  posts.forEach((post) => {
+    feed.item({
+      title: post.title,
+      description: post.title,
+      date: new Date(post.date),
+      url: `https://moeki.dev/blog/${post.id}`,
+    });
+  });
+
+  return feed.xml();
+}
