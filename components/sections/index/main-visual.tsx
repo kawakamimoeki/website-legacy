47f9cbe5f0d25016f44b060ab96bc859297c89af
Moeki Kawakami <me@moeki.dev>
2023-09-04 14:10:46 +0900

    update

--- components/sections/index/main-visual.tsx
+++ components/sections/index/main-visual.tsx
@@ -4,13 +4,13 @@ import Image from 'next/image'
 
 export default function Mainvisual() {
   return (
-    <>
+    <div className="mb-5">
       <Title>
         <Link href="/">
           <span className="">@moekidev</span>
         </Link>
       </Title>
       <p>{"Hello. I'm a web developer in Japan."}</p>
-    </>
+    </div>
   )
 }
